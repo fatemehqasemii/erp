0a97e5e650e12961d7312e92e87f85dfea749520
f.ghasemi <hello.fatemeh1@gmail.com>
2025-01-26 17:15:18 +0330

    fix tailwind config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Nunito } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const nunito = Nunito({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: "--font-nunito",
 });
 
 export const metadata: Metadata = {
@@ -27,11 +22,7 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/favicon.svg" />
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${nunito.variable} `}>{children}</body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,50 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      colors: {},
+      fontSize: {
+        xs: [
+          "12px",
+          {
+            lineHeight: "16.37px",
+            fontWeight: "800",
+          },
+        ],
+        sm: [
+          "14px",
+          {
+            lineHeight: "24px",
+            fontWeight: "400",
+          },
+        ],
+        base: [
+          "16px",
+          {
+            lineHeight: "21.82px",
+            fontWeight: "800",
+          },
+        ],
+        lg: [
+          "20px",
+          {
+            lineHeight: "27.28px",
+            fontWeight: "800px",
+          },
+        ],
+        "2lg": [
+          "24px",
+          {
+            lineHeight: "32.74px",
+            fontWeight: "800",
+          },
+        ],
+        "2xl": [
+          "28px",
+          {
+            lineHeight: "38.19px",
+            fontWeight: "800",
+          },
+        ],
       },
     },
   },
